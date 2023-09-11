45b4dd9002260fadbae35a7a3f9f79ef69ce9051
Michelle Pinar <michelle-pinar@Michelles-MacBook-Air.local>
2023-09-10 22:46:57 -0400

    whatever...

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# school-backend
+# Fullstack-School
--- services.js
+++ services.js
@@ -2,13 +2,14 @@
 // IMPORTING PACKAGES AND DECLARATION OF CONSTANTS
 //------------------------------------------------------------------------------
 const express = require("express");
+const { Database } = require("sqlite3");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
 
 app.use(express.json());
 
 //app.use(express.urlencoded());
-const port = 8080;
+const port = 8000;
 //------------------------------------------------------------------------------
 // DATABASE FUNCTIONS
 //------------------------------------------------------------------------------
@@ -61,6 +62,7 @@ closeDataBaseConnection(dataBase);
 //------------------------------------------------------------------------------
 // Endpoints management
 //------------------------------------------------------------------------------
+//----------------------- Post a new student -------------------------//
 app.post("/student", (request, response) => {
   const dataBase = openDataBaseConnection();
   const { firstName, lastName, age, sex } = request.body;
@@ -91,7 +93,7 @@ app.post("/student", (request, response) => {
     closeDataBaseConnection(dataBase);
   });
 });
-
+//------------------------ Get ALL students --------------------------//
 app.get("/students", (request, response) => {
   const dataBase = openDataBaseConnection();
   const query = "SELECT * FROM students";
@@ -101,22 +103,81 @@ app.get("/students", (request, response) => {
       console.error(err.message);
       response.status(500).json({ error: "Error retrieving students" });
     } else {
-      response.json(rows);
+      response.status(200).json(rows);
       console.log("Here's the full list of students we got!");
     }
     // Close the database connection after the operation is completed
     closeDataBaseConnection(dataBase);
   });
 });
-
-app.delete("/student/:id", (request, response) => {
+//----------------------- Get student by ID --------------------------//
+app.get("/student/:studentId", (request, response) => {
   const dataBase = openDataBaseConnection();
-  const student = student.find((c) => c.Id === parseInt(request.params.Id));
-  if (!student) return response.status(404).send("Student not found");
+  const studentId = request.params.studentId;
+  const query = "SELECT firstName,lastName,age,sex FROM students WHERE id=?;";
+  const value = [studentId];
 
-  const index = student.indexOf(student);
-  student.splice(index, 1);
-  response.send(student);
+  dataBase.get(query, value, (err, row) => {
+    if (err) {
+      console.error(err.message);
+      response.status(500).json({ error: "Error getting student" });
+    } else {
+      if (row === undefined || row === null)
+        row = "no data for student with this specific id exists";
+      response.status(200).json({
+        message: "Getting student successful with the id = " + studentId,
+        student: row,
+      });
+      console.log("Here's the student with the specific id = " + studentId);
+    }
+    // Close the database connection after the operation is completed
+    closeDataBaseConnection(dataBase);
+  });
+});
+
+//----------------------- Delete student --------------------------//
+app.delete("/student/:studentId", (request, response) => {
+  const dataBase = openDataBaseConnection();
+  const studentId = request.params.studentId;
+  const value = [studentId];
+  const query = "DELETE FROM students WHERE id=?;";
+
+  dataBase.run(query, value, (err, row) => {
+    if (err) {
+      console.error(err.message);
+      response.status(500).json({
+        error: "Error while trying to delete student with id=" + studentId,
+      });
+    } else {
+      if (row === undefined || row === null)
+        row = "no data for student with this specific id exists";
+      response.status(200).json({
+        message: "Student with the id = " + studentId + " as been deleted",
+        student: row,
+      });
+      console.log("Student with ID = " + studentId + " successfully deleted");
+    }
+  });
+});
+//--------------------- Updating student -----------------------------
+app.put("/student/updateFirstName/:studentId", (request, response) => {
+  const dataBase = openDataBaseConnection();
+  const studentId = request.params.studentId;
+  const value = [studentId, newData];
+  const newData = request.body.newData;
+  const query = "UPDATE students SET firstName  WHERE id=?;";
+
+  dataBase.run(query, value, (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return response.status(500).json({
+        error: "Error while updating student with id=" + studentId,
+      });
+    } else {
+      console.log("Updated data ${this.changes} row(?)");
+      response.json({ message: "Data updated succesfully" });
+    }
+  });
 });
 //------------------------------------------------------------------------------
 // Close the database connection when the server is shutting down
